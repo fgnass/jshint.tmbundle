c476763446f1bb4e60a143ba2b0401ad0cb81bde
Rob (Robot) Middleton <rob@i.tv>
2011-02-20 20:19:48 -0700

    Moved jshint options to bundle file, added arrow pointing to error and changed to use require.

--- Support/jshint.js
+++ Support/jshint.js
@@ -1,44 +1,39 @@
 var sys = require("sys"),
 	fs = require("fs"),
-	Script = process.binding('evals').Script,
 	env = process.env || process.ENV,
-	file = env.TM_FILEPATH;
+	file = env.TM_FILEPATH,
+	JSHINT = require(__dirname + '/fulljshint.js').JSHINT;
 
 var input = fs.readFileSync(file, 'utf8');
 
 //remove shebang
 input = input.replace(/^\#\!.*/, "");
 
-Script.runInThisContext(fs.readFileSync(__dirname + '/fulljshint.js', 'utf8'));
-
-var success = JSHINT(input, {
-	es5: true,
-	predef: [
-		// CommonJS
-		"exports", 
-		"require",
-		"module",
-		// NodeJS
-		"GLOBAL",
-		"process",
-		"__filename",
-		"__dirname"
-	]
-});
-
-if (!success) {
-	var body = '';
-	JSHINT.errors.forEach(function(e) {
-		if (e) {
-			body += ('<a href="txmt://open?url=file://' + escape(file) + '&line=' + e.line + '&column=' + e.character + '">' + e.reason);
-			if (e.evidence) {
-				//TODO highlight column
-				body += '<pre>' + (e.evidence || '') + '</pre></a>';
-			}
-		}
-	});
-	fs.readFile(__dirname + '/output.html', 'utf8', function(e, html) {
-		sys.puts(html.replace('{body}', body));
-		process.exit(205); //show_html
-	});
+function repeat (str, num) {
+  return new Array(num + 1).join(str);
 }
+
+module.exports = function (options) {
+    var success = JSHINT(input, options);
+
+    if (!success) {
+        var body = '';
+        JSHINT.errors.forEach(function(e) {
+            if (e) {
+                body += ('<a href="txmt://open?url=file://' + escape(file) + '&line=' + e.line + '&column=' + e.character + '">' + e.reason);
+                if (e.evidence) {
+                    //TODO highlight column
+                    body += '<pre>' + (e.evidence || '') + '</pre>';
+                    if (e.character) {
+                        body += '<pre>' + repeat(' ', e.character - 1) + '^</pre>';
+                    }
+                    body += '</a>';
+                }
+            }
+        });
+        fs.readFile(__dirname + '/output.html', 'utf8', function(e, html) {
+            sys.puts(html.replace('{body}', body));
+            process.exit(205); //show_html
+        });
+    }
+};
