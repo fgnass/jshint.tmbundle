b5ce0b68a257eeca3cdf00f7c5089c18f44d5616
Anthony <anthony@nicefrontend.com>
2011-04-26 23:25:15 +0100

    Added dot notation suggestion to warnings list

--- Support/jshint-tm.js
+++ Support/jshint-tm.js
@@ -13,7 +13,8 @@ var sys = require("sys"),
 var warningMsgs = [
 	'Missing semicolon.', 
 	'Mixed spaces and tabs.',
-	'The body of a for in should be wrapped in an if statement to filter unwanted properties from the prototype'
+	'The body of a for in should be wrapped in an if statement to filter unwanted properties from the prototype',
+	'is better written in dot notation'
 ];
 
 function inArray(item, arry) {
